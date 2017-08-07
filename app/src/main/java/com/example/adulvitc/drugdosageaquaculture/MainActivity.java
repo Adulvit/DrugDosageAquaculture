cdb2e12dd57f347adf8ce10d19b9cbcbbc6d9de5
Em <adulvitc@gmail.com>
2017-08-07 09:26:47 -0700

    Initial commit

---
+++ app/src/main/java/com/example/adulvitc/drugdosageaquaculture/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.adulvitc.drugdosageaquaculture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView drugListView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+    } // Main Method
+
+
+
+
+
+
+}  // Main class
